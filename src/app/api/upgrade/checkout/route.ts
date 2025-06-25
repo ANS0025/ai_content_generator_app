fd951936ed3005152759fd9b2e9a50e258d85094
Akihito Niko Seki <75423028+ANS0025@users.noreply.github.com>
2025-06-25 23:00:55 +0900

    add: stripe and db integration

--- src/app/api/upgrade/checkout/route.ts
+++ src/app/api/upgrade/checkout/route.ts
@@ -1,27 +1,75 @@
-import Stripe from 'stripe';
-import { db } from '@/lib/db';
-import { auth } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
+import Stripe from "stripe";
+import { db } from "@/lib/db";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2025-05-28.basil",
+});
 
-export async funtion POST(req: Request) {
+export async function POST(req: Request) {
   try {
     const { userId } = auth();
+    const user = await currentUser();
     if (!userId) {
       return new NextResponse("User not Authenticated", { status: 401 });
     }
 
-    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [{
-      quantity: 1,
-      price_data: {
-        currency: 'JPY',
-        product_data: {
-          name: '10,000 AI Credit',
-          description: 'Unlock 10,000 AI Credits',
+    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [
+      {
+        quantity: 1,
+        price_data: {
+          currency: "JPY",
+          product_data: {
+            name: "10,000 AI Credit",
+            description: "Unlock 10,000 AI Credits",
+          },
+          unit_amount: 1000,
         },
-        unit_amount: 1000
       },
-    }];
+    ];
+
+    const purchase = await db.purchase.create({
+      data: {
+        userId: userId,
+        credit: 10000,
+      },
+    });
+
+    let stripeCustomer = await db.stripeCustomer.findUnique({
+      where: {
+        userId: userId,
+      },
+      select: {
+        stripeCustomerId: true,
+      },
+    });
+
+    if (!stripeCustomer) {
+      const customer = await stripe.customers.create({
+        email: user?.emailAddresses[0].emailAddress,
+      });
+
+      stripeCustomer = await db.stripeCustomer.create({
+        data: {
+          userId: userId,
+          stripeCustomerId: customer.id,
+        },
+      });
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      customer: stripeCustomer.stripeCustomerId,
+      line_items,
+      mode: "payment",
+      success_url: `${process.env.NEXT_PUBLIC_APP_URL}/dashboard`,
+      cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/`,
+      metadata: {
+        purchaseId: purchase.id,
+      },
+    });
+
+    return NextResponse.json({ url: session.url });
   } catch (error) {
     console.log(error);
     return new NextResponse("Internal Server Error", { status: 500 });
